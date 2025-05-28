870a5f9bdd5a4e65153f7aca4f2d6eaf80bb8ce4
hosono <hjun105106@hotmail.co.jp>
2025-05-29 01:01:56 +0900

    ポート設定を環境変数から取得するように変更

--- server.js
+++ server.js
@@ -172,4 +172,5 @@ app.get('/api/get-history', async (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log('Server started'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('Server started on port ' + port));
